ad5cc8a23cd2608aa5e18aef9ac485105eeaf209
Abdel <abdelhakmerabtene@gmail.com>
2024-10-30 01:14:27 +0100

    Insert data into 'MaisonMannequin' table

--- Scripts_SQL/projet.sql
+++ Scripts_SQL/projet.sql
@@ -270,3 +270,115 @@ CREATE TABLE Participer (
     FOREIGN KEY (nDefile) REFERENCES Defile(nDefile) ON DELETE CASCADE,
     FOREIGN KEY (nTenue) REFERENCES Tenue(nTenue) ON DELETE SET NULL
 );
+
+
+---------------------------------
+
+-- INSERTION A LA TABLE MaisonMode
+
+
+---------------------------------
+
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Chanel', 'Gabrielle Chanel', TO_DATE('1910-01-01', 'YYYY-MM-DD'), 'Paris, France', 'chanel_logo.png', 'https://www.chanel.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Dior', 'Christian Dior', TO_DATE('1946-10-08', 'YYYY-MM-DD'), 'Paris, France', 'dior_logo.png', 'https://www.dior.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Louis Vuitton', 'Louis Vuitton', TO_DATE('1854-01-01', 'YYYY-MM-DD'), 'Paris, France', 'lv_logo.png', 'https://www.louisvuitton.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Gucci', 'Guccio Gucci', TO_DATE('1921-01-01', 'YYYY-MM-DD'), 'Florence, Italy', 'gucci_logo.png', 'https://www.gucci.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Prada', 'Mario Prada', TO_DATE('1913-01-01', 'YYYY-MM-DD'), 'Milan, Italy', 'prada_logo.png', 'https://www.prada.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Versace', 'Gianni Versace', TO_DATE('1978-01-01', 'YYYY-MM-DD'), 'Milan, Italy', 'versace_logo.png', 'https://www.versace.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Hermes', 'Thierry Hermes', TO_DATE('1837-01-01', 'YYYY-MM-DD'), 'Paris, France', 'hermes_logo.png', 'https://www.hermes.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Balenciaga', 'Cristóbal Balenciaga', TO_DATE('1919-01-01', 'YYYY-MM-DD'), 'Paris, France', 'balenciaga_logo.png', 'https://www.balenciaga.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Saint Laurent', 'Yves Saint Laurent', TO_DATE('1961-01-01', 'YYYY-MM-DD'), 'Paris, France', 'ysl_logo.png', 'https://www.ysl.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Givenchy', 'Hubert de Givenchy', TO_DATE('1952-01-01', 'YYYY-MM-DD'), 'Paris, France', 'givenchy_logo.png', 'https://www.givenchy.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Burberry', 'Thomas Burberry', TO_DATE('1856-01-01', 'YYYY-MM-DD'), 'London, UK', 'burberry_logo.png', 'https://www.burberry.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Valentino', 'Valentino Garavani', TO_DATE('1960-01-01', 'YYYY-MM-DD'), 'Rome, Italy', 'valentino_logo.png', 'https://www.valentino.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Armani', 'Giorgio Armani', TO_DATE('1975-01-01', 'YYYY-MM-DD'), 'Milan, Italy', 'armani_logo.png', 'https://www.armani.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Ralph Lauren', 'Ralph Lauren', TO_DATE('1967-01-01', 'YYYY-MM-DD'), 'New York, USA', 'ralphlauren_logo.png', 'https://www.ralphlauren.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Fendi', 'Adele and Edoardo Fendi', TO_DATE('1925-01-01', 'YYYY-MM-DD'), 'Rome, Italy', 'fendi_logo.png', 'https://www.fendi.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Lacoste', 'Rene Lacoste', TO_DATE('1933-01-01', 'YYYY-MM-DD'), 'Paris, France', 'lacoste_logo.png', 'https://www.lacoste.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Kenzo', 'Kenzo Takada', TO_DATE('1970-01-01', 'YYYY-MM-DD'), 'Paris, France', 'kenzo_logo.png', 'https://www.kenzo.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Moschino', 'Franco Moschino', TO_DATE('1983-01-01', 'YYYY-MM-DD'), 'Milan, Italy', 'moschino_logo.png', 'https://www.moschino.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Bottega Veneta', 'Michele Taddei', TO_DATE('1966-01-01', 'YYYY-MM-DD'), 'Vicenza, Italy', 'bottega_logo.png', 'https://www.bottegaveneta.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Dolce Gabbana', 'Domenico Dolce Stefano Gabbana', TO_DATE('1985-01-01', 'YYYY-MM-DD'), 'Milan, Italy', 'dg_logo.png', 'https://www.dolcegabbana.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Salvatore Ferragamo', 'Salvatore Ferragamo', TO_DATE('1927-01-01', 'YYYY-MM-DD'), 'Florence, Italy', 'ferragamo_logo.png', 'https://www.ferragamo.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Tommy Hilfiger', 'Tommy Hilfiger', TO_DATE('1985-01-01', 'YYYY-MM-DD'), 'New York, USA', 'tommy_logo.png', 'https://www.tommy.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Marc Jacobs', 'Marc Jacobs', TO_DATE('1986-01-01', 'YYYY-MM-DD'), 'New York, USA', 'marcjacobs_logo.png', 'https://www.marcjacobs.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Alexander McQueen', 'Alexander McQueen', TO_DATE('1992-01-01', 'YYYY-MM-DD'), 'London, UK', 'mcqueen_logo.png', 'https://www.alexandermcqueen.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Oscar de la Renta', 'Oscar de la Renta', TO_DATE('1965-01-01', 'YYYY-MM-DD'), 'New York, USA', 'oscar_logo.png', 'https://www.oscardelarenta.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Vera Wang', 'Vera Wang', TO_DATE('1990-01-01', 'YYYY-MM-DD'), 'New York, USA', 'verawang_logo.png', 'https://www.verawang.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Ermenegildo Zegna', 'Ermenegildo Zegna', TO_DATE('1910-01-01', 'YYYY-MM-DD'), 'Trivero, Italy', 'zegna_logo.png', 'https://www.zegna.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Miu Miu', 'Miuccia Prada', TO_DATE('1993-01-01', 'YYYY-MM-DD'), 'Milan, Italy', 'miumiu_logo.png', 'https://www.miumiu.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Alberta Ferretti', 'Alberta Ferretti', TO_DATE('1980-01-01', 'YYYY-MM-DD'), 'Milan, Italy', 'alberta_logo.png', 'https://www.albertaferretti.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Thom Browne', 'Thom Browne', TO_DATE('2001-01-01', 'YYYY-MM-DD'), 'New York, USA', 'thombrowne_logo.png', 'https://www.thombrowne.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Haider Ackermann', 'Haider Ackermann', TO_DATE('2003-01-01', 'YYYY-MM-DD'), 'Paris, France', 'haider_logo.png', 'https://www.haiderackermann.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Isabel Marant', 'Isabel Marant', TO_DATE('1994-01-01', 'YYYY-MM-DD'), 'Paris, France', 'isabel_logo.png', 'https://www.isabelmarant.com');
+
+INSERT INTO MaisonMode (nomMaisonMode, nomFondateur, dateFondation, localisation, logo, siteWEB) 
+VALUES ('Ann Demeulemeester', 'Ann Demeulemeester', TO_DATE('1985-01-01', 'YYYY-MM-DD'), 'Antwerp, Belgium', 'ann_logo.png', 'https://www.anndemeulemeester.com');
+
+
+
+
