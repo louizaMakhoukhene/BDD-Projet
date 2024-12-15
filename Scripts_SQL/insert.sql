0574f180683062e0873ac605dcd6e65b0f1f864a
Abdel <abdelhakmerabtene@gmail.com>
2024-12-15 17:14:27 +0100

    Simple insertion : participer

--- Scripts_SQL/insert.sql
+++ Scripts_SQL/insert.sql
@@ -763,3 +763,33 @@ VALUES (30, 40.0, 950.00, 'Robe Rubis', 'Robe luxueuse aux tons rouges profonds.
 
 
 -- INSERTION A LA TABLE participer 
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (1, 1, 1);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (2, 2, 2);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (3, 3, 3);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (4, 4, 4);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (5, 5, 5);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (6, 6, 6);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (7, 7, 7);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (8, 8, 8);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (9, 9, 9);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (10, 10, 10);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (11, 11, 11);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (12, 12, 12);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (13, 13, 13);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (14, 14, 14);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (15, 15, 15);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (16, 16, 16);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (17, 17, 17);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (18, 18, 18);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (19, 19, 19);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (20, 20, 20);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (21, 21, 21);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (22, 22, 22);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (23, 23, 23);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (24, 24, 24);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (25, 25, 25);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (26, 26, 26);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (27, 27, 27);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (28, 28, 28);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (29, 29, 29);
+INSERT INTO Participer (nMannequin, nDefile, nTenue) VALUES (30, 30, 30);
